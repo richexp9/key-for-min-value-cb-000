f0935bf8ef7b7a8229fd6f70c37a71bbabf7e81a
Richard Loera <richexp9@gmx.com>
2018-01-16 05:42:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   smallest = nil
-    value = ''
+    minvalue = ''
   if name_hash == {}
     return nil
   else
